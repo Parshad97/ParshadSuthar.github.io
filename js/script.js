4d60bb13f183549854c0bdcb8c4c256272e48c19
Parshad Suthar <92554035+Parshad97@users.noreply.github.com>
2025-09-15 13:23:26 -0500

    Update-3

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ window.onscroll = () => {
         if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
             });
         }
     });
@@ -45,7 +45,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-info', { origin: 'bottom' });
+ScrollReveal().reveal('.image-container, .projects-container, .projects-box, .contact-info', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
